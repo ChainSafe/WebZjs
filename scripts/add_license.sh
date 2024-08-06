18ff39fa8a6d1adcbe409282bb5a7d3e6f1c572c
Willem Olding <willemolding@gmail.com>
2024-08-06 17:05:19 -0400

    add script to add copyright header

---
+++ scripts/add_license.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Checks if the source code contains required license and adds it if necessary.
+# Returns 1 if there was a missing license, 0 otherwise.
+
+PAT_APA="^// Copyright 2024 ChainSafe Systems// SPDX-License-Identifier: Apache-2.0, MIT$"
+
+ret=0
+for file in $(git grep --cached -Il '' -- '*.rs'); do
+  header=$(head -2 "$file" | tr -d '\n')
+	if ! echo "$header" | grep -q "$PAT_APA"; then
+		echo "$file was missing header"
+		cat ./scripts/copyright.txt "$file" > temp
+		mv temp "$file"
+		ret=1
+	fi
+done
+
+exit $ret
