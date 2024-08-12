dee4ec11dfd52f6b36aeb8d2234809afe12a32d0
Willem Olding <willemolding@gmail.com>
2024-08-12 12:08:18 -0400

    fmt to make runner pass

--- src/store/memory_store.rs
+++ src/store/memory_store.rs
@@ -1,8 +1,8 @@
 // Copyright 2024 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use crate::store::WalletStore;
 use crate::error::Error;
+use crate::store::WalletStore;
 
 /// A simple in-memory store for wallet data. Useful for testing
 pub struct MemoryStore {
