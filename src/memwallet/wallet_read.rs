4f3ed5e921b2ed8ba2d090274f6c679a29783f92
Eric Tu <tu.eric@hotmail.com>
2024-08-13 15:14:09 -0400

    split into files

---
+++ src/memwallet/wallet_read.rs
@@ -0,0 +1,266 @@
+// #![allow(unused)]
+use incrementalmerkletree::{Address, Marking, Retention};
+use sapling::NullifierDerivingKey;
+use secrecy::{ExposeSecret, SecretVec};
+use shardtree::{error::ShardTreeError, store::memory::MemoryShardStore, ShardTree};
+use std::{
+    cmp::Ordering,
+    collections::{BTreeMap, HashMap, HashSet},
+    convert::Infallible,
+    hash::Hash,
+    num::NonZeroU32,
+};
+use zcash_keys::keys::{AddressGenerationError, DerivationError, UnifiedIncomingViewingKey};
+use zip32::{fingerprint::SeedFingerprint, DiversifierIndex, Scope};
+
+use zcash_primitives::{
+    block::BlockHash,
+    consensus::{BlockHeight, Network},
+    legacy::TransparentAddress,
+    transaction::{Transaction, TxId},
+    zip32::AccountId,
+};
+use zcash_protocol::{
+    memo::{self, Memo, MemoBytes},
+    value::Zatoshis,
+    ShieldedProtocol::{Orchard, Sapling},
+};
+
+use zcash_client_backend::{
+    address::UnifiedAddress,
+    keys::{UnifiedAddressRequest, UnifiedFullViewingKey, UnifiedSpendingKey},
+    wallet::{NoteId, TransparentAddressMetadata, WalletSpend, WalletTransparentOutput, WalletTx},
+};
+
+use super::*;
+use zcash_client_backend::data_api::{
+    chain::ChainState, chain::CommitmentTreeRoot, scanning::ScanRange, Account, AccountBirthday,
+    AccountPurpose, AccountSource, BlockMetadata, DecryptedTransaction, NullifierQuery,
+    ScannedBlock, SeedRelevance, SentTransaction, WalletCommitmentTrees, WalletRead, WalletSummary,
+    WalletWrite, ORCHARD_SHARD_HEIGHT, SAPLING_SHARD_HEIGHT,
+};
+
+impl WalletRead for MemoryWalletDb {
+    type Error = Error;
+    type AccountId = u32;
+    type Account = MemAccount;
+
+    fn get_account_ids(&self) -> Result<Vec<Self::AccountId>, Self::Error> {
+        Ok(self.accounts.keys().copied().collect())
+    }
+
+    fn get_account(
+        &self,
+        account_id: Self::AccountId,
+    ) -> Result<Option<Self::Account>, Self::Error> {
+        Ok(self
+            .accounts
+            .get(&account_id)
+            .map(|account| (account_id, account.ufvk.clone()).into()))
+    }
+
+    fn get_derived_account(
+        &self,
+        _seed: &SeedFingerprint,
+        _account_id: zip32::AccountId,
+    ) -> Result<Option<Self::Account>, Self::Error> {
+        todo!()
+    }
+
+    fn validate_seed(
+        &self,
+        _account_id: Self::AccountId,
+        _seed: &SecretVec<u8>,
+    ) -> Result<bool, Self::Error> {
+        todo!()
+    }
+
+    fn seed_relevance_to_derived_accounts(
+        &self,
+        seed: &SecretVec<u8>,
+    ) -> Result<SeedRelevance<Self::AccountId>, Self::Error> {
+        todo!()
+    }
+
+    fn get_account_for_ufvk(
+        &self,
+        ufvk: &UnifiedFullViewingKey,
+    ) -> Result<Option<Self::Account>, Self::Error> {
+        let ufvk_req =
+            UnifiedAddressRequest::all().expect("At least one protocol should be enabled");
+        Ok(self.accounts.iter().find_map(|(id, acct)| {
+            if acct.ufvk.default_address(ufvk_req).unwrap()
+                == ufvk.default_address(ufvk_req).unwrap()
+            {
+                Some((*id, acct.ufvk.clone()).into())
+            } else {
+                None
+            }
+        }))
+    }
+
+    fn get_current_address(
+        &self,
+        account: Self::AccountId,
+    ) -> Result<Option<UnifiedAddress>, Self::Error> {
+        self.accounts
+            .get(&account)
+            .map(|account| {
+                account
+                    .ufvk
+                    .default_address(
+                        UnifiedAddressRequest::all()
+                            .expect("At least one protocol should be enabled."),
+                    )
+                    .map(|(addr, _)| addr)
+            })
+            .transpose()
+            .map_err(|e| e.into())
+    }
+
+    fn get_account_birthday(&self, account: Self::AccountId) -> Result<BlockHeight, Self::Error> {
+        self.accounts
+            .get(&account)
+            .map(|account| account.birthday.height())
+            .ok_or_else(|| Error::AccountUnknown(account))
+    }
+
+    fn get_wallet_birthday(&self) -> Result<Option<BlockHeight>, Self::Error> {
+        self.accounts
+            .values()
+            .map(|account| account.birthday.height())
+            .min()
+            .ok_or(Error::AccountUnknown(0))
+            .map(Some)
+    }
+
+    fn get_wallet_summary(
+        &self,
+        _min_confirmations: u32,
+    ) -> Result<Option<WalletSummary<Self::AccountId>>, Self::Error> {
+        todo!()
+    }
+
+    fn chain_height(&self) -> Result<Option<BlockHeight>, Self::Error> {
+        todo!()
+    }
+
+    fn get_block_hash(&self, block_height: BlockHeight) -> Result<Option<BlockHash>, Self::Error> {
+        Ok(self.blocks.iter().find_map(|b| {
+            if b.0 == &block_height {
+                Some(b.1.hash)
+            } else {
+                None
+            }
+        }))
+    }
+
+    fn block_metadata(&self, _height: BlockHeight) -> Result<Option<BlockMetadata>, Self::Error> {
+        todo!()
+    }
+
+    fn block_fully_scanned(&self) -> Result<Option<BlockMetadata>, Self::Error> {
+        todo!()
+    }
+
+    fn get_max_height_hash(&self) -> Result<Option<(BlockHeight, BlockHash)>, Self::Error> {
+        todo!()
+    }
+
+    fn block_max_scanned(&self) -> Result<Option<BlockMetadata>, Self::Error> {
+        todo!()
+    }
+
+    fn suggest_scan_ranges(&self) -> Result<Vec<ScanRange>, Self::Error> {
+        Ok(vec![])
+    }
+
+    fn get_target_and_anchor_heights(
+        &self,
+        _min_confirmations: NonZeroU32,
+    ) -> Result<Option<(BlockHeight, BlockHeight)>, Self::Error> {
+        todo!()
+    }
+
+    fn get_min_unspent_height(&self) -> Result<Option<BlockHeight>, Self::Error> {
+        todo!()
+    }
+
+    fn get_tx_height(&self, txid: TxId) -> Result<Option<BlockHeight>, Self::Error> {
+        self.tx_idx
+            .get(&txid)
+            .copied()
+            .map(Some)
+            .ok_or(Error::TxUnknown(txid))
+    }
+
+    fn get_unified_full_viewing_keys(
+        &self,
+    ) -> Result<HashMap<Self::AccountId, UnifiedFullViewingKey>, Self::Error> {
+        Ok(HashMap::new())
+    }
+
+    fn get_memo(&self, id_note: NoteId) -> Result<Option<Memo>, Self::Error> {
+        self.tx_idx
+            .get(id_note.txid())
+            .and_then(|height| self.blocks.get(height))
+            .and_then(|block| block.memos.get(&id_note))
+            .map(Memo::try_from)
+            .transpose()
+            .map_err(Error::from)
+    }
+
+    fn get_transaction(&self, _id_tx: TxId) -> Result<Option<Transaction>, Self::Error> {
+        todo!()
+    }
+
+    fn get_sapling_nullifiers(
+        &self,
+        query: NullifierQuery,
+    ) -> Result<Vec<(Self::AccountId, sapling::Nullifier)>, Self::Error> {
+        // self.sapling_spends
+        //     .iter()
+        //     .filter(|s| match query {
+        //         NullifierQuery::All => true,
+        //         NullifierQuery::Unspent => !s.1 .1,
+        //     })
+        //     .map(|(nullifier, (txid, _))| {
+        //         self.accounts.iter().find_map(|(id, acct)| {
+        //             let dfvk = acct.ufvk.sapling().ok()?;
+        //             let nk = dfvk.to_nk(Scope::External);
+        //             let note = nk.to_note_from_nullifier(*nullifier);
+        //             Some((*id, note))
+        //         })
+        //     })
+        //     .collect()
+        todo!()
+    }
+
+    fn get_orchard_nullifiers(
+        &self,
+        _query: NullifierQuery,
+    ) -> Result<Vec<(Self::AccountId, orchard::note::Nullifier)>, Self::Error> {
+        todo!()
+    }
+
+    fn get_transparent_receivers(
+        &self,
+        _account: Self::AccountId,
+    ) -> Result<HashMap<TransparentAddress, Option<TransparentAddressMetadata>>, Self::Error> {
+        todo!()
+    }
+
+    fn get_transparent_balances(
+        &self,
+        _account: Self::AccountId,
+        _max_height: BlockHeight,
+    ) -> Result<HashMap<TransparentAddress, Zatoshis>, Self::Error> {
+        todo!()
+    }
+
+    fn transaction_data_requests(
+        &self,
+    ) -> Result<Vec<zcash_client_backend::data_api::TransactionDataRequest>, Self::Error> {
+        todo!()
+    }
+}
