b29d8cefea3420d6d35f819ccb11eaa0b47da695
Eric Tu <tu.eric@hotmail.com>
2024-09-13 12:39:21 -0400

    can spawn_blocking and then spawn a rayon task in a fork join scope inside it

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,24 +5,28 @@ use zcash_address::ZcashAddress;
 use zcash_primitives::consensus::Network;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
-
+macro_rules! console_log {
+    ($($t:tt)*) => (web_sys::console::log_1(&format!($($t)*).into()))
+}
 const SEED: &str = "visit armed kite pen cradle toward reward clay marble oil write dove blind oyster silk oyster original message skate bench tone enable stadium element";
 const HD_INDEX: u32 = 0;
 const BIRTHDAY: Option<u32> = Some(2577329);
 
 const THREADS: usize = 5;
+use tokio_with_wasm::alias as tokio;
 
+static INIT: Once = Once::new();
 // Required to initialize the logger and panic hooks only once
 use std::{num::NonZeroU32, sync::Once};
-static INIT: Once = Once::new();
 pub fn initialize() {
+    console_log!("Calling initialize");
     INIT.call_once(|| {
         webz_core::init::start();
     });
 }
 #[cfg(all(feature = "wasm-parallel"))]
 async fn init_threadpool(threads: usize) -> wasm_bindgen_futures::JsFuture {
-    tracing::info!("Initializing thread pool with {} threads", threads);
+    console_log!("Initializing thread pool with {} threads", threads);
     wasm_bindgen_futures::JsFuture::from(wasm_bindgen_rayon::init_thread_pool(threads))
 }
 
@@ -31,10 +35,7 @@ async fn test_get_and_scan_range() {
     initialize();
 
     #[cfg(all(feature = "wasm-parallel"))]
-    let _ = init_threadpool(THREADS).await;
-
-    let num_parallel = rayon::current_num_threads();
-    tracing::info!("WASM rayon has {} threads", num_parallel);
+    init_threadpool(THREADS).await;
 
     let mut w = WebWallet::new("test", "https://zcash-testnet.chainsafe.dev", 1).unwrap();
 
@@ -54,19 +55,33 @@ async fn test_get_and_scan_range() {
     tracing::info!("Wallet summary: {:?}", summary);
 
     tracing::info!("Proposing a transaction");
+    let handle = tokio::task::spawn_blocking(|| {
+        rayon::scope(|s| {
+            s.spawn(|_| {
+                let num_parallel = rayon::current_num_threads();
+                tracing::info!("WASM rayon has {} threads", num_parallel);
+            })
+        });
+    });
     w.transfer(SEED, 0, "utest1z00xn09t4eyeqw9zmjss75sf460423dymgyfjn8rtlj26cffy0yad3eea82xekk24s00wnm38cvyrm2c6x7fxlc0ns4a5j7utgl6lchvglfvl9g9p56fqwzvzvj9d3z6r6ft88j654d7dj0ep6myq5duz9s8x78fdzmtx04d2qn8ydkxr4lfdhlkx9ktrw98gd97dateegrr68vl8xu".to_string(), 1000).await.unwrap();
     tracing::info!("Transaction proposed");
 
     let summary = w.get_wallet_summary().unwrap();
     tracing::info!("Wallet summary: {:?}", summary);
+
+    handle.await.unwrap();
 }
 
 #[cfg(feature = "native")]
 #[tokio::test]
 async fn test_get_and_scan_range_native() {
     initialize();
-    let num_parallel = rayon::current_num_threads();
-    tracing::info!("Native rayon has {} threads", num_parallel);
+
+    rayon::spawn(|| {
+        let num_parallel = rayon::current_num_threads();
+        tracing::info!("Native rayon has {} threads", num_parallel);
+    });
+
     let url = "https://testnet.zec.rocks:443";
     let c = tonic::transport::Channel::from_shared(url).unwrap();
 
