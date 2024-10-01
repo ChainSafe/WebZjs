38aa204601c5bd87ccef533eeae7b10d938c25c5
Willem Olding <willemolding@gmail.com>
2024-10-01 14:28:55 -0400

    fmt

--- src/bindgen/proposal.rs
+++ src/bindgen/proposal.rs
@@ -5,7 +5,7 @@ use zcash_primitives::transaction::fees::zip317::FeeRule;
 
 /// A handler to an immutable proposal. This can be passed to `create_proposed_transactions` to prove/authorize the transactions
 /// before they are sent to the network.
-/// 
+///
 /// The proposal can be reviewed by calling `describe` which will return a JSON object with the details of the proposal.
 #[wasm_bindgen]
 pub struct Proposal {
