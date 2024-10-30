441a49b812fa6686cc9945a374618433153e7b06
Anton Lykhoyda <lykhoyda@gmail.com>
2024-10-30 17:33:36 +0100

    add tailwind config + aliases

--- packages/web-wallet/vite.config.ts
+++ packages/web-wallet/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+});
