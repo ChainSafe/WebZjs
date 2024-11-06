b9749efb9b5b392264f7dcd0f71f10ff94973939
Willem Olding <willemolding@gmail.com>
2024-11-07 10:06:58 +1100

    add extra logic to select env var style depending on bundler (vite or parcel)

--- packages/demo-wallet/src/config/snap.ts
+++ packages/demo-wallet/src/config/snap.ts
@@ -1,3 +1,5 @@
+import { env } from './env_selector';
+
 export const defaultSnapOrigin =
   // eslint-disable-next-line no-restricted-globals
-  import.meta.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+  env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
