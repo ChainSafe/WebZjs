6c16b8e99d990e194bb9d70d2481df9fbd747238
Anton Lykhoyda <lykhoyda@gmail.com>
2024-12-03 17:22:55 +0100

    cleanup

--- packages/web-wallet/src/assets/index.ts
+++ packages/web-wallet/src/assets/index.ts
@@ -1,6 +1,6 @@
 import ChainsafeSvg from './chainsafe.svg';
 import FormTransferSvg from './form-transfer.svg';
-import MetaMaskLogoSvg from './metaMaskLogo.svg';
+import MetaMaskLogoSvg from './metaMask-logo.svg';
 import MetaMaskSnapsLogoSvg from './metamask-snaps-logo.svg';
 import ZcashSvg from './zcash.svg';
 import ZcashYellowSvg from './zcash-yellow.svg';
