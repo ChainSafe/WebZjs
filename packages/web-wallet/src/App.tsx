03b1f60e15340c1c5c88775b38a9ffcfe5e01c21
Anton Lykhoyda <lykhoyda@gmail.com>
2025-01-06 17:37:39 +0100

    update bg

--- packages/web-wallet/src/App.tsx
+++ packages/web-wallet/src/App.tsx
@@ -1,13 +1,20 @@
 import { useInterval } from 'usehooks-ts';
 import { useWebZjsActions } from '@hooks/useWebzjsActions.ts';
 import Layout from '@components/Layout/Layout.tsx';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { RESCAN_INTERVAL } from './config/constants.ts';
+import { useEffect } from 'react';
 
 function App() {
   const { triggerRescan } = useWebZjsActions();
+  const location = useLocation();
 
-  // rescan the wallet periodically
+  useEffect(() => {
+    // Add custom background to home page
+    document.body.classList.remove('home-page-bg');
+    if (location.pathname === '/') document.body.classList.add('home-page-bg');
+  }, [location]);
+
   useInterval(() => {
     triggerRescan();
   }, RESCAN_INTERVAL);
