0fc9065b97906450539aabe9c7f21d7e1ef39a90
Anton Lykhoyda <lykhoyda@gmail.com>
2025-02-24 19:28:49 +0100

    fix config

--- packages/web-wallet/src/config/constants.ts
+++ packages/web-wallet/src/config/constants.ts
@@ -1,13 +1,7 @@
 export const LIGHTWALLET_PROXY =
-  process.env.LIGHTWALLET_PROXY ?? 'https://zcash-mainnet.chainsafe.dev';
-export const WALLET_NETWORK = process.env.WALLET_ENV ?? 'main';
+  process.env.PARCEL_PUBLIC_LIGHTWALLET_PROXY ??
+  'https://zcash-mainnet.chainsafe.dev';
+export const WALLET_NETWORK = process.env.PARCEL_PUBLIC_WALLET_ENV ?? 'main';
 export const ZATOSHI_PER_ZEC = 1e8;
 export const RESCAN_INTERVAL = 20000;
 export const NU5_ACTIVATION = 1687104;
-
-console.log('process.env', process.env.NODE_ENV);
-console.log(
-  'process.env.LIGHTWALLET_PROXY',
-  process.env.PARCEL_PUBLIC_LIGHTWALLET_PROXY,
-);
-console.log('process.env.WALLET_NETWORK', process.env.PARCEL_PUBLIC_WALLET_ENV);
